1058caddaf8bf24ef4177854259057febdcb35fb
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-20 10:31:05 +0100

    version 1.2

--- src/main/java/com/ashkiano/fakepluginscommand/FakePluginsCommand.java
+++ src/main/java/com/ashkiano/fakepluginscommand/FakePluginsCommand.java
@@ -11,6 +11,7 @@ public class FakePluginsCommand extends JavaPlugin {
     @Override
     public void onEnable() {
         Metrics metrics = new Metrics(this, 21289);
+        this.getLogger().info("Thank you for using the FakePluginsCommand plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
         this.getCommand("plugins").setExecutor(new CommandExecutor() {
             @Override
             public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
